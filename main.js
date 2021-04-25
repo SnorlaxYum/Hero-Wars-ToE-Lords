501f540ea3b07ecf99ba4e9a085edf02896cafea
Sim <sim@snorl.ax>
2021-04-26 01:15:23 +0800

    lord-video-add wrong name

--- main.js
+++ main.js
@@ -89,7 +89,7 @@ client.on("message", msg => {
                 }
                 // get the last insert id
                 console.log(`A row has been inserted with rowid ${this.lastID}`)
-                replyQueryMessages(`Successfully added the video for ${videoArray[3]} from ${videoArray[2]}`)
+                replyQueryMessages(`Successfully added the video for ${videoArray[3]} from ${videoArray[0]}`)
             })
         }
     } else if (msg.content === "!lord-time") {
