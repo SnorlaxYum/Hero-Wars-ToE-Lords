50e47772d5100f1cc01e7ba325963266fbf09cd9
Sim <sim@snorl.ax>
2021-04-25 20:15:21 +0800

    weekJudge

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({restRequestTimeout:150000})
 
+function weekJudge() {
+    let pos = (new Date() - new Date('2021-04-12T13:00:00+0800'))
+    let week = pos / (7* 24 * 60 * 60 * 1000) % 3
+    let weekday = (new Date() - new Date('2021-04-12T13:00:00+0800')) % (7* 24 * 60 * 60 * 1000) / (24*60*60*1000)
+    return {week, weekday}
+}
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
-  }
+    if (msg.content === "ping") {
+        msg.reply("pong");
+    } else if (msg.content === "lords") {
+        const {week, weekday} = weekJudge()
+        msg.content(`Week ${week<1 ? 'A' : week < 2 ? 'B': 'C'} Day ${parseInt(weekday)+1}`)
+    }
 })
 
 try{
