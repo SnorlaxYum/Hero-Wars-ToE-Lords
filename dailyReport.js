c0de317eb70cc3a76b2b3310194023bd142b34ed
Sim <sim@snorl.ax>
2021-04-26 19:21:24 +0800

    dailyreport thing

--- dailyReport.js
+++ dailyReport.js
@@ -10,24 +10,23 @@ client.on("ready", () => {
 try{
     client.login(process.env.TOKEN).then(res => {
         recordLog("Login Request success")
-        const {week, weekday, time} = weekJudge()
+        const {week, weekday} = weekJudge()
         let channels = [...client.channels.cache.values()].filter(ch => ch.name === 'toe-daily')
         if(!channels) return
         if(channels) {
             channels.forEach(channel => {
-                // dailyComboQuery(week, weekday).then(res => {
-                //     if(typeof res === "string") {
-                //         channel.send(res)
-                //     } else {
-                //         channel.send(res[0]+'\n'+res[1][0].join('\n'))
-                //         for(let i = 1; i < res[1].length; i++) {
-                //             channel.send(res[1][i].join('\n'))
-                //         }
-                //     }
-                // }, rej => {
-                //     channel.send(rej)
-                // })
-                channel.send(`test isolation, ${week}, ${weekday}`).then(msg => msg.delete())
+                dailyComboQuery(week, weekday).then(res => {
+                    if(typeof res === "string") {
+                        channel.send(res)
+                    } else {
+                        channel.send(res[0]+'\n'+res[1][0].join('\n'))
+                        for(let i = 1; i < res[1].length; i++) {
+                            channel.send(res[1][i].join('\n'))
+                        }
+                    }
+                }, rej => {
+                    channel.send(rej)
+                })
             })
         }
     }, rej => {
