0df5e75ae324e7212b9db11b6df0e342d87b664d
Sim <sim@snorl.ax>
2021-04-27 16:57:43 +0800

    shortcut mistake

--- util.js
+++ util.js
@@ -5,7 +5,7 @@
  * @returns {String[]} converted shortcut
  */
 function youtubeToShortcut(uri) {
-    return /youtube.com\/watch\?v\=(\w+)([\s\S]+)/.exec(uri).slice(1) || /youtu.be\/(\w+)([\s\S]+)/.exec(uri)[1].slice(1)
+    return /youtube.com\/watch\?v\=(\w+)([\s\S]+)/.exec(uri) ? /youtube.com\/watch\?v\=(\w+)([\s\S]+)/.exec(uri).slice(1) : /youtu.be\/(\w+)([\s\S]+)/.exec(uri)[1].slice(1)
 }
 
 /**
