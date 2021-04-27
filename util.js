00892f3c855f7b90e9c099f84545b4eaf9fe2fea
Sim <sim@snorl.ax>
2021-04-27 17:06:17 +0800

    sc rules

--- util.js
+++ util.js
@@ -5,7 +5,7 @@
  * @returns {String[]} converted shortcut
  */
 function youtubeToShortcut(uri) {
-    return /youtube.com\/watch\?v\=(\w+)([\s\S]+)/.exec(uri) ? /youtube.com\/watch\?v\=(\w+)([\s\S]+)/.exec(uri).slice(1) : /youtu.be\/(\w+)([\s\S]+)/.exec(uri)[1].slice(1)
+    return /youtube.com\/watch\?v\=([A-Za-z0-9_\-]+)([\s\S]+)/.exec(uri) ? /youtube.com\/watch\?v\=([A-Za-z0-9_\-]+)([\s\S]+)/.exec(uri).slice(1) : /youtu.be\/([A-Za-z0-9_\-]+)([\s\S]+)/.exec(uri)[1].slice(1)
 }
 
 /**
@@ -14,7 +14,7 @@ function youtubeToShortcut(uri) {
  * @returns {String} the uri of the youtube video
  */
 function shortcutToYoutube(sc, param='') {
-    return `https://youtu.be/${/youtube:(\w+)/.exec(sc)[1]}${param}`
+    return `https://youtu.be/${/youtube:([A-Za-z0-9_\-]+)/.exec(sc)[1]}${param}`
 }
 
 module.exports = {youtubeToShortcut, shortcutToYoutube}
