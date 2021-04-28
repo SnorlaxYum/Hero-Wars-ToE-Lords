87ab642fdf1337fffd7196a9e782ac60b7ad8658
Sim <sim@snorl.ax>
2021-04-28 17:00:22 +0800

    saperate video add and delete functions

--- utils/command.js
+++ utils/command.js
@@ -4,6 +4,15 @@ const { replyQueryMessagesWrapperImport, sendMessagesWrapperImport, adminPermiss
 const { GuildMember, Message, MessageEmbed, TextChannel, StringResolvable, APIMessage } = require("./discord").Discord
 
 /**
+ * check if the member has admin permission
+ * @param {GuildMember} guildMember guild member info object
+ * @returns {Boolean} whether he has the admin permission
+ */
+function adminPermission(guildMember) {
+    return adminPermissionImport(guildMember.roles.cache.values(), guildMember.guild.id)
+}
+
+/**
  * help command, manual for the bot user
  * @param {String} msgCon message content
  * @param {(msg: MessageEmbed) => void} sendMessages function responsible for sending message
@@ -29,6 +38,82 @@ function commandHelp(msgCon, sendMessages) {
 }
 
 /**
+ * add a lord video
+ * @param {String} msgCon message content
+ * @param {GuildMember} guildMember guild member info object
+ * @param {(msg: StringResolvable|APIMessage) => void} replyMessage function responsible for replying the message
+ */
+function commandVideoAdd(msgCon, guildMember, replyMessage) {
+    if (adminPermission(guildMember)) {
+        const videoArray = msgCon.split("[+++]").slice(1)
+        videoArray[0] = /\w+/.exec(videoArray[0])[0]
+        if(videoArray[0] !== "All") {
+            try {
+                videoArray[1] = comboParser(videoArray[1])
+                videoArray[3] = comboParser(videoArray[3])
+            } catch(e) {
+                return replyMessage(e.message)
+            }
+        }
+        videoArray[4] = parseInt(videoArray[4])
+        
+        // video uri convert
+        let videoFinaluri = getVideoShortcut(videoArray[5])
+        videoArray[5] = videoFinaluri[0]
+        videoArray.push(videoFinaluri[1])
+
+        if (videoArray.length < 6) {
+            replyMessage('need 6 parameters (lord text, combo text, player text, attackingCombo text, point integer, uri text)')
+        } else {
+            addLordVideo(videoArray, err => {
+                if (err) {
+                    console.error(err.message)
+                    if(err.message.indexOf("UNIQUE constraint failed") !== -1) {
+                        replyMessage("the video is already in the database.")
+                    } else {
+                        replyMessage("an internal error happened.")
+                    }
+                } else {
+                    console.info(`A row has been inserted into video with uri ${videoArray[5]}`)
+                    replyMessage(`successfully added the video for ${videoArray[1]} from ${videoArray[2]}`)
+                }
+            })
+        }
+    } else {
+        replyMessage("sorry, you have no permissions to complete this action.")
+    }
+}
+
+/**
+ * delete lord videos from the given uris
+ * @param {String} msgCon message content
+ * @param {GuildMember} guildMember guild member info object
+ * @param {(msg: StringResolvable|APIMessage) => void} replyMessage function responsible for replying the message
+ */
+function commandVideoDelete(msgCon, guildMember, replyMessage) {
+    if (adminPermission(guildMember)) {
+        let videoArray = msgCon.split(" ").slice(1)
+        if (videoArray.length === 0) {
+            replyMessage('this API needs at least one uri to complete')
+        } else {
+            videoArray = videoArray.map(uri => {
+                return getVideoShortcut(uri)[0]
+            })
+            deleteLordVideos(videoArray, err => {
+                if (err) {
+                    replyMessage(err.message)
+                } else {
+                    console.info(`Successfully deleted the videos whose uri is ${videoArray.join(' or ')}`)
+                    replyMessage(`successfully deleted the videos whose uri is ${videoArray.join(' or ')}`)
+                }
+            })
+        }
+    } else {
+        replyMessage("sorry, you have no permissions to complete this action.")
+    }
+}
+
+/**
  * parse command string and get the job done.
  * @param {String} msgCon message content
  * @param {TextChannel} msgChannel message content
@@ -43,72 +128,12 @@ function commandCenter(msgCon, msgChannel, guildMember, replyMessage, deleteMess
     function sendMessages(content, delNotification=true, timeout = 60 * 1000) {
         sendMessagesWrapperImport(content, delNotification, msgChannel, o => deleteMessage(o), timeout)
     }
-    function adminPermission() {
-        return adminPermissionImport(guildMember.roles.cache.values(), guildMember.guild.id)
-    }
     if (msgCon === "!ping") {
         replyQueryMessages("pong")
     } else if (msgCon.startsWith("!lord-video-add")) {
-        if (adminPermission()) {
-            const videoArray = msgCon.split("[+++]").slice(1)
-            videoArray[0] = /\w+/.exec(videoArray[0])[0]
-            if(videoArray[0] !== "All") {
-                try {
-                    videoArray[1] = comboParser(videoArray[1])
-                    videoArray[3] = comboParser(videoArray[3])
-                } catch(e) {
-                    return replyQueryMessages(e.message)
-                }
-            }
-            videoArray[4] = parseInt(videoArray[4])
-            
-            // video uri convert
-            let videoFinaluri = getVideoShortcut(videoArray[5])
-            videoArray[5] = videoFinaluri[0]
-            videoArray.push(videoFinaluri[1])
-
-            if (videoArray.length < 6) {
-                replyQueryMessages('need 6 parameters (lord text, combo text, player text, attackingCombo text, point integer, uri text)')
-            } else {
-                addLordVideo(videoArray, err => {
-                    if (err) {
-                        console.error(err.message)
-                        if(err.message.indexOf("UNIQUE constraint failed") !== -1) {
-                            replyQueryMessages("the video is already in the database.")
-                        } else {
-                            replyQueryMessages("an internal error happened.")
-                        }
-                    } else {
-                        console.info(`A row has been inserted into video with uri ${videoArray[5]}`)
-                        replyQueryMessages(`successfully added the video for ${videoArray[1]} from ${videoArray[2]}`)
-                    }
-                })
-            }
-        } else {
-            replyQueryMessages("sorry, you have no permissions to complete this action.")
-        }
-
+        commandVideoAdd(msgCon, guildMember, replyQueryMessages)
     } else if (msgCon.startsWith("!lord-video-delete")) {
-        if (adminPermission()) {
-            let videoArray = msgCon.split(" ").slice(1)
-            if (videoArray.length === 0) {
-                replyQueryMessages('this API needs at least one uri to complete')
-            } else {
-                videoArray = videoArray.map(uri => {
-                    return getVideoShortcut(uri)[0]
-                })
-                deleteLordVideos(videoArray, err => {
-                    if (err) {
-                        replyQueryMessages(err.message)
-                    } else {
-                        console.info(`Successfully deleted the videos whose uri is ${videoArray.join(' or ')}`)
-                        replyQueryMessages(`successfully deleted the videos whose uri is ${videoArray.join(' or ')}`)
-                    }
-                })
-            }
-        } else {
-            replyQueryMessages("sorry, you have no permissions to complete this action.")
-        }
+        commandVideoDelete(msgCon, guildMember, replyQueryMessages)
     } else if (msgCon === "!lord-time") {
         const { week, weekday, time } = weekJudge(), padNum = num => String(num).padStart(2, "0")
         let timeTotalSec = parseInt(time / 1000), second = timeTotalSec % 60, min = parseInt(timeTotalSec / 60) % 60, hour = parseInt(timeTotalSec / 60 / 60)
