5e22a7042ce531f454cfe5432522945482f2de1d
Sim <sim@snorl.ax>
2021-04-30 17:36:20 +0800

    judge lord-time from string

--- commands/lord-time.js
+++ commands/lord-time.js
@@ -12,7 +12,16 @@ module.exports = {
      * @param {Discord.Message} msg query message
      */
     exec(args, msg) {
-        const { week, weekday, time } = weekJudge(), padNum = num => String(num).padStart(2, "0")
+        const padNum = num => String(num).padStart(2, "0")
+        let week, weekday, time
+        try {
+            let curPos = args[0] ? weekJudge(args[0]) : weekJudge()
+            week = curPos.week
+            weekday = curPos.weekday
+            time = curPos.time
+        } catch(e) {
+            throw e
+        }
         let timeTotalSec = parseInt(time / 1000), second = timeTotalSec % 60, min = parseInt(timeTotalSec / 60) % 60, hour = parseInt(timeTotalSec / 60 / 60)
         timeoutDeleteMessage(msg, `Week ${week} - Day ${weekday} - ${padNum(hour)}:${padNum(min)}:${padNum(second)}`, true) 
     }
